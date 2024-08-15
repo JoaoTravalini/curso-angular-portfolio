5957cb566765503b57694a9bab76b1feda5f3493
Joao Travalini <joao.m.travalini@gmail.com>
2024-08-15 10:56:04 -0300

    Imagens arrumadas, 52 terminado

--- src/app/modules/portfolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { Iknowledge } from '../../interface/IKnowledge.interface';
 
 @Component({
   selector: 'app-knowledge',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './knowledge.component.html',
   styleUrl: './knowledge.component.scss'
 })
 export class KnowledgeComponent {
-
+  public arrayKnowledge = signal<Iknowledge[]>([
+    {
+      src: 'assets/icons/knowledge/html5.svg',
+      alt: 'Ícone de conhecimento de html5'
+    },
+    {
+      src: 'assets/icons/knowledge/css3.svg',
+      alt: 'Ícone de conhecimento de css3'
+    },
+    {
+      src: 'assets/icons/knowledge/typescript.svg',
+      alt: 'Ícone de conhecimento de typescript'
+    },
+    {
+      src: 'assets/icons/knowledge/angular.svg',
+      alt: 'Ícone de conhecimento de angular'
+    },
+    {
+      src: 'assets/icons/knowledge/nodejs.svg',
+      alt: 'Ícone de conhecimento de nodejs'
+    },
+  ])
 }
