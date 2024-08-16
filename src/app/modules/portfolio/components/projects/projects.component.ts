059f30f2c4c0881b7eb5d551be35af812953af28
Joao Travalini <joao.m.travalini@gmail.com>
2024-08-16 15:06:56 -0300

    Aula 63 finalizada

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,15 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { IProjects } from '../../interface/IProjects.interface';
+
+import { MatDialog, MatDialogModule } from '@angular/material/dialog'
+
+import { EdialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: "assets/img/uflajr.png",
@@ -96,4 +103,11 @@ export class ProjectsComponent {
       ],
     },
   ]);
+
+  public openDialog(data: IProjects){
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EdialogPanelClass.PROJECTS
+    })
+  }
 }
