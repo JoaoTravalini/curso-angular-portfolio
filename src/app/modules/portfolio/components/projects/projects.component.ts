2cc7333f1d3a67f8c8436f4d3c78bd5c15d656d6
Joao Travalini <joao.m.travalini@gmail.com>
2024-08-16 14:26:13 -0300

    Aula 61 finalizada

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -22,8 +22,78 @@ export class ProjectsComponent {
         {
           name: 'Conheça o Site',
           href: 'https://uflajunior.com'
-        }
-      ]
-    }
+        },
+      ],
+    },
+    {
+      src: "assets/img/uflajr.png",
+      alt: 'Protótipo - E-educa',
+      title: 'E-Educa',
+      width: '63px',
+      height: '50px',
+      description: 'Explore a fascinante prototipagem realizada para a empresa E-Educa.',
+      links: [
+        {
+          name: 'Conheça o Site',
+          href: 'https://aplicaprova-f3d36.web.app'
+        },
+      ],
+    },
+    {
+      src: "assets/img/uflajr.png",
+      alt: 'Site Institucional - UFLA Júnior',
+      title: 'UFLA Júnior',
+      width: '63px',
+      height: '50px',
+      description: 'Explore o fascinante site insticucional realizado para a empresa júnior UFLA Jr.',
+      links: [
+        {
+          name: 'Conheça o Site',
+          href: 'https://uflajunior.com'
+        },
+      ],
+    },
+    {
+      src: "assets/img/uflajr.png",
+      alt: 'Site Institucional - UFLA Júnior',
+      title: 'UFLA Júnior',
+      width: '63px',
+      height: '50px',
+      description: 'Explore o fascinante site insticucional realizado para a empresa júnior UFLA Jr.',
+      links: [
+        {
+          name: 'Conheça o Site',
+          href: 'https://uflajunior.com'
+        },
+      ],
+    },
+    {
+      src: "assets/img/uflajr.png",
+      alt: 'Site Institucional - UFLA Júnior',
+      title: 'UFLA Júnior',
+      width: '63px',
+      height: '50px',
+      description: 'Explore o fascinante site insticucional realizado para a empresa júnior UFLA Jr.',
+      links: [
+        {
+          name: 'Conheça o Site',
+          href: 'https://uflajunior.com'
+        },
+      ],
+    },
+    {
+      src: "assets/img/uflajr.png",
+      alt: 'Site Institucional - UFLA Júnior',
+      title: 'UFLA Júnior',
+      width: '63px',
+      height: '50px',
+      description: 'Explore o fascinante site insticucional realizado para a empresa júnior UFLA Jr.',
+      links: [
+        {
+          name: 'Conheça o Site',
+          href: 'https://uflajunior.com'
+        },
+      ],
+    },
   ]);
 }
