99af89dd21a0aafbc3d1ba496fc36f9bcae5ac99
Joao Travalini <joao.m.travalini@gmail.com>
2024-08-17 15:07:51 -0300

    Portfolio e curso finalizados

--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
 import { ExperiencesComponent } from '../../components/experiences/experiences.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
+import { FormacaoComponent } from '../../components/formacao/formacao.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, KnowledgeComponent, ExperiencesComponent, ProjectsComponent],
+  imports: [HeaderComponent, KnowledgeComponent, ExperiencesComponent, ProjectsComponent, FormacaoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
